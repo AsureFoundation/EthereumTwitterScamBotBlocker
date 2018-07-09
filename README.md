7a5b1fd83d181632d2b49b91193a1b00810b2258
Paul Mizel <email@pmizel.de>
2018-07-10 00:47:52 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# EthereumTwitterScamBotBlocker
-Ethereum Twitter ScamBot Blocker
+# Ethereum Twitter ScamBot Blocker
+
+Scambots are annoying.
+
+**Want to add a ScamBot?**
+ 
+ Change [twitter.json](twitter.json) file.
+
+### Chrome Web 
+
+![Google Chrome WebStore](Chromewebstore.png?raw=true)
+
+[Google Chrome WebStore](https://chrome.google.com/webstore/detail/clclnlfjnmbbljbodkkjhphdmjgjmbio)
+
+
+
--- contentScript.js
+++ contentScript.js
@@ -1,36 +1,41 @@
-
 $.getJSON("https://raw.githubusercontent.com/AsureFoundation/EthereumTwitterScamBotBlocker/master/twitter.json", function(response) {
 
 	var json = response;
 	this.json = json;
+	var action = "red";
+	var counter = 0;
+	chrome.storage.sync.get(['action'], function(result) {
+		console.log('action currently is ' + result.action);
+        action = result.action || "red";
+	});
 	
 	function modify(){
-		chrome.storage.sync.get(['action'], function(result) {
-			console.log('action currently is ' + result.action);
-        	var action = result.action || "red";
-			
-			$('.tweet').each(function(index){
-				var t = $(this).html();
-				var jt=$(this);
-				console.log(jt.attr("data-user-id") +" - "+jt.attr("data-screen-name"));
-				var blacklist=json.blacklist;			
-				if(blacklist[jt.attr("data-user-id")]!==undefined){
-					if(action==="hide")
-					{jt.hide();}
-					else
-					{jt.css("background-color","red");}					
-				}
-			});
-		});
+		$('.tweet').each(function(index){
+			var jt = $(this);			
+			var blacklist = json.blacklist;			
+			if(blacklist[jt.attr("data-user-id")]!==undefined){
+				console.log("hit: "+jt.attr("data-user-id") +" - "+jt.attr("data-screen-name"));
+				jt.attr("data-etsbb","true");				
+				if(action==="hide")
+				{jt.hide();}
+				else
+				{jt.css("background-color","red");}					
+			}
+		});		
+		counter = $('.tweet[data-etsbb="true"]').length;
+		if(counter>0)
+		{
+			var tweetsText = $("ul.ProfileNav-list li.ProfileNav-item--tweets span.ProfileNav-value");
+			var number=tweetsText.attr("data-count");
+			tweetsText.html(number+'/<span style="color:red">'+counter+'</span>');
+		}		
 	}
-
-	function DOMModificationHandler(){
-		$(this).unbind('DOMSubtreeModified.event1');
-		setTimeout(function(){
-			modify();
-			$('#timeline').bind('DOMSubtreeModified.event1',DOMModificationHandler);
-		},10);
+	
+	function setIntervalAndExecute(fn, t) {
+		fn();
+		return(setInterval(fn, t));
 	}
+		
+	var i = setIntervalAndExecute(modify, 3000);
 
-	$('#timeline').bind('DOMSubtreeModified.event1',DOMModificationHandler);						   
 });
